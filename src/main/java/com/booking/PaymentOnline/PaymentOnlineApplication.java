a8f4134c617e178a9a9138211462a3c44e008872
quanminh.ngo <quanminh.ngo@dxc.com>
2022-12-11 12:13:34 +0700

    Commit Code

---
+++ src/main/java/com/booking/PaymentOnline/PaymentOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.booking.PaymentOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/PaymentOnline/PaymentOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.PaymentOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
